3fdc781f920f80f14f96a866f6a56319b3486f15
riturajs19 <rituraj.singh.met23@itbhu.ac.in>
2025-06-17 18:18:51 +0530

    Third commit

--- MCQ_Generator.py
+++ MCQ_Generator.py
@@ -1,7 +1,13 @@
-import spacy
 import random
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
-nlp = spacy.load("en_core_web_sm")
+
 
 def extract_nouns(text):
     doc = nlp(text)
--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Update package list and install Tesseract OCR
 apt-get update
 apt-get install -y tesseract-ocr
 
+# Create Streamlit config folder
 mkdir -p ~/.streamlit/
 
+# Set Streamlit server configuration
 echo "\
 [server]\n\
 headless = true\n\
@@ -12,4 +15,8 @@ port = \$PORT\n\
 enableCORS = false\n\
 " > ~/.streamlit/config.toml
 
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Download spaCy model
 python -m spacy download en_core_web_sm
